1e73d858ffc035be647c233eb146e9a7abc47b6f
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-09-03 13:34:42 +0200

    Initial commit

---
+++ Template/_products.php
@@ -0,0 +1,85 @@
+<!--   product  -->
+<?php
+    $item_id = $_GET['item_id'] ?? 1;
+    foreach ($product->getData() as $item) :
+        if ($item['item_id'] == $item_id) :
+?>
+<section id="product" class="py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="<?php echo $item['item_image'] ?? "./assets/products/1.png" ?>" alt="product" class="img-fluid">
+            </div>
+            <div class="col-sm-6 py-5">
+                <h5 class="font-baloo font-size-20"><?php echo $item['item_name'] ?? "Unknown"; ?></h5>
+                <small>by <?php echo $item['item_brand'] ?? "Brand"; ?></small>
+                <div class="d-flex">
+                    <div class="rating text-warning font-size-12">
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="far fa-star"></i></span>
+                    </div>
+        
+                </div>
+                <hr class="m-0">
+
+                <!---    product price       -->
+                <table class="my-3">
+                    <tr class="font-rale font-size-14">
+                        <td>M.R.P:</td>
+                        <td><strike>$162.00</strike></td>
+                    </tr>
+                    <tr class="font-rale font-size-14">
+                        <td>Deal Price:</td>
+                        <td class="font-size-20 text-danger">$<span><?php echo $item['item_price'] ?? 0; ?></span><small class="text-dark font-size-12">&nbsp;&nbsp;Inclusive of all taxes</small></td>
+                    </tr>
+                    
+                </table>
+                <!---    !product price       -->
+
+                <!--    #policy -->
+                <div id="policy">
+                    <div class="d-flex">
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-retweet border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-rale font-size-12">10 Days <br> Replacement</a>
+                        </div>
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-truck  border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-rale font-size-12">Daily Tuition <br>Deliverd</a>
+                        </div>
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-check-double border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-rale font-size-12">1 Year <br>Warranty</a>
+                        </div>
+                    </div>
+                </div>
+                <!--    !policy -->
+                <hr>
+
+               
+
+            </div>
+
+            <div class="col-12">
+                <h6 class="font-rubik p-100">Product Description</h6>
+                <hr>
+                <p class="font-rale font-size-14">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat inventore vero numquam error est ipsa, consequuntur temporibus debitis nobis sit, delectus officia ducimus dolorum sed corrupti. Sapiente optio sunt provident, accusantium eligendi eius reiciendis animi? Laboriosam, optio qui? Numquam, quo fuga. Maiores minus, accusantium velit numquam a aliquam vitae vel?</p>
+                <p class="font-rale font-size-14">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat inventore vero numquam error est ipsa, consequuntur temporibus debitis nobis sit, delectus officia ducimus dolorum sed corrupti. Sapiente optio sunt provident, accusantium eligendi eius reiciendis animi? Laboriosam, optio qui? Numquam, quo fuga. Maiores minus, accusantium velit numquam a aliquam vitae vel?</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--   !product  -->
+<?php
+        endif;
+        endforeach;
+?>
